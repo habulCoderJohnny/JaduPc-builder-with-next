ec2238c3f1e9d5b3536ac9f4e4dc09af159d722f
habulCoderJohnny <fokruljohnny@gmail.com>
2023-08-05 23:21:02 +0600

    Footer update

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
   const gmailHandler = () => {
     window.open(
       "mailto:" +
-      "programmer.hazratali@gmail.com" +
+      "fjohnnydeveloper@gmail.com" +
       "?subject=" +
       " " +
       "&body=" +
@@ -53,7 +53,7 @@ function Footer() {
                 className="cursor-pointer"
                 alt="linkedin"
                 onClick={() => {
-                  router.push("https://www.linkedin.com/in/hazrat-ali");
+                  router.push("https://www.linkedin.com/in/fokruljohnny");
                 }}
               />
             </div>
@@ -65,7 +65,7 @@ function Footer() {
                 objectFit="contain"
                 className="cursor-pointer"
                 alt="github"
-                onClick={() => router.push("https://github.com/hazratali")}
+                onClick={() => router.push("https://github.com/habulCoderJohnny")}
               />
             </div>
           </div>
@@ -73,7 +73,7 @@ function Footer() {
         <p className="mt-6 text-gray-200 text-center flex items-center flex-wrap justify-center">
           Made with <HeartIcon className="w-5 mx-2 text-red-500" /> by
           <span className="text-white hover:underline ml-2">
-            <Link href="https://github.com/habulCoderJohnny">Johnny Dev</Link>
+            <Link href="https://www.showwcase.com/habluthegreat">Johnny Dev</Link>
           </span>
         </p>
       </div>
